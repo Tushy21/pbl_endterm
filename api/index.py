8854bd8e2e90bec6a98b322b9205ccdc525abf97
Tushar Chauhan <chauhantushar2005@gmail.com>
2026-04-14 12:13:04 +0530

    Comply with Vercel experimentalServices monorepo requirement

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Add the root directory to path so that we can import backend.src
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from backend.src.app import app
